ad64d0a78a4b012d2031f35d7ca65325df418afa
Parker Windows <69760520+psmithdev@users.noreply.github.com>
2025-01-09 20:51:36 -0600

    Add React Router for navigation and create Home, About, and Contact pages

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,31 @@
 import React from "react";
 import TypeformEmbed from "./components/TypeformEmbed";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import { Link } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Chakra CheckIn</h1>
-      <TypeformEmbed />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+      <div className="App">
+        <h1>Chakra CheckIn</h1>
+        <TypeformEmbed />
+      </div>
+    </Router>
   );
 };
 
